496c3159000fc2ad8dd3649aba82c7506679edbf
hehai123 <lim.xin.da@hotmail.com>
2023-07-05 20:08:15 +0800

    Updated cube probility, changed css

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import Button from '@material-ui/core/Button';
-import MenuIcon from '@material-ui/icons/Menu';
-import PotentialTable from './potentialTable';
-import PercentageGraphs from './percentageGraphs';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
+import MenuIcon from "@material-ui/icons/Menu";
+import PotentialTable from "./potentialTable";
+import PercentageGraphs from "./percentageGraphs";
 
 import { useHistory } from "react-router-dom";
-import ShouldITodd from './shouldITodd';
+import ShouldITodd from "./shouldITodd";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   menuButton: {
     marginRight: theme.spacing(2),
   },
@@ -20,7 +20,7 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1,
   },
   button: {
-    width: '120px',
+    width: "120px",
   },
 }));
 
@@ -31,7 +31,6 @@ const MainPage = () => {
 
   const handleOpen = () => {
     setOpen(!open);
-
   };
 
   const handleClose = () => {
@@ -39,9 +38,9 @@ const MainPage = () => {
   };
 
   return (
-    <div>
+    <>
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar style={{ backgroundColor: "#f25c54" }}>
           <IconButton
             edge="start"
             color="inherit"
@@ -51,22 +50,23 @@ const MainPage = () => {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            Maple Math with Shitty UI
+            Maple Math with <del>Shitty</del> Okay UI
           </Typography>
-          <Button className={classes.button} color="inherit" onClick={handleOpen}>
-            {open ?
+          <Button
+            className={classes.button}
+            color="inherit"
+            onClick={handleOpen}
+          >
+            {open ? (
               <Typography>TODD COST CALCULATOR</Typography>
-              :
+            ) : (
               <Typography>CUBE RATES CALCULATOR</Typography>
-            }
+            )}
           </Button>
         </Toolbar>
       </AppBar>
-      {open ?
-        <PotentialTable />
-        :
-        <ShouldITodd />}
-    </div>
+      {open ? <PotentialTable /> : <ShouldITodd />}
+    </>
   );
 };
 
---
+++ src/components/uniqueLines.js
@@ -0,0 +1,748 @@
+//Armor
+
+export const uniqueHatLines = [
+  {
+    stat: "9% Str",
+    type: "STR",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Dex",
+    type: "DEX",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Int",
+    type: "INT",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Luk",
+    type: "LUK",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% HP",
+    type: "HP",
+    value: 9,
+    weight: 6,
+  },
+  {
+    stat: "9% MP",
+    type: "MP",
+    value: 9,
+    weight: 6,
+  },
+  {
+    stat: "9% Def",
+    type: "DEF",
+    value: 9,
+    weight: 4,
+  },
+  {
+    stat: "6% ALL STATS",
+    type: "AS",
+    value: 6,
+    weight: 4,
+  },
+  {
+    stat: "5% chance to ignore 20% damage when hit",
+    type: "Chance to Ignore DMG",
+    value: 20,
+    weight: 4,
+  },
+  {
+    stat: "5% chance to ignore 40% damage when hit",
+    type: "Chance to Ignore DMG",
+    value: 40,
+    weight: 4,
+  },
+  {
+    stat: "Increase efficiency of HP recovery items and skills by 30%",
+    type: "REC",
+    weight: 4,
+  },
+  {
+    stat: "Decent Mystic Door",
+    type: "Decent Skill",
+    value: 1,
+    weight: 4,
+  },
+];
+
+export const uniqueTopLines = [
+  {
+    stat: "9% Str",
+    type: "STR",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Dex",
+    type: "DEX",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Int",
+    type: "INT",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Luk",
+    type: "LUK",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% HP",
+    type: "HP",
+    value: 9,
+    weight: 6,
+  },
+  {
+    stat: "9% MP",
+    type: "MP",
+    value: 9,
+    weight: 6,
+  },
+  {
+    stat: "9% Def",
+    type: "DEF",
+    value: 9,
+    weight: 4,
+  },
+  {
+    stat: "6% ALL STATS",
+    type: "AS",
+    value: 6,
+    weight: 4,
+  },
+  {
+    stat: "5% chance to ignore 20% damage when hit",
+    type: "Chance to Ignore DMG",
+    value: 20,
+    weight: 4,
+  },
+  {
+    stat: "5% chance to ignore 40% damage when hit",
+    type: "Chance to Ignore DMG",
+    value: 40,
+    weight: 4,
+  },
+  {
+    stat: "Invincibility time +2 seconds when hit",
+    type: "Chance to Ignore DMG",
+    value: 2,
+    weight: 4,
+  },
+  {
+    stat: "2% chance of being invincible for 7 seconds when hit",
+    type: "Chance of being invincibile",
+    value: 2,
+    weight: 4,
+  },
+  {
+    stat: "30% chance to reflect 50% of damage taken",
+    type: "Reflect Damage",
+    value: 50,
+    weight: 4,
+  },
+  {
+    stat: "30% chance to reflect 70% of damage taken",
+    type: "Reflect Damage",
+    value: 70,
+    weight: 2,
+  },
+  {
+    stat: "Increase efficiency of HP recovery items and skills by 30%",
+    type: "REC",
+    value: 30,
+    weight: 4,
+  },
+];
+
+export const uniquebottomShoeLines = [
+  {
+    stat: "9% Str",
+    type: "STR",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Dex",
+    type: "DEX",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Int",
+    type: "INT",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Luk",
+    type: "LUK",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% HP",
+    type: "HP",
+    value: 9,
+    weight: 6,
+  },
+  {
+    stat: "9% MP",
+    type: "MP",
+    value: 9,
+    weight: 6,
+  },
+  {
+    stat: "9% Def",
+    type: "DEF",
+    value: 9,
+    weight: 4,
+  },
+  {
+    stat: "6% ALL STATS",
+    type: "AS",
+    value: 6,
+    weight: 4,
+  },
+  {
+    stat: "5% chance to ignore 20% damage when hit",
+    type: "Chance to Ignore DMG",
+    value: 20,
+    weight: 4,
+  },
+  {
+    stat: "5% chance to ignore 40% damage when hit",
+    type: "Chance to Ignore DMG",
+    value: 40,
+    weight: 4,
+  },
+  {
+    stat: "Increase efficiency of HP recovery items and skills by 30%",
+    type: "REC",
+    value: 30,
+    weight: 4,
+  },
+  {
+    stat: "Decent Hyper Body",
+    type: "Decent Skill",
+    value: 1,
+    weight: 4,
+  },
+];
+
+export const uniqueGloveLines = [
+  {
+    stat: "9% Str",
+    type: "STR",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Dex",
+    type: "DEX",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Int",
+    type: "INT",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Luk",
+    type: "LUK",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% HP",
+    type: "HP",
+    value: 9,
+    weight: 6,
+  },
+  {
+    stat: "9% MP",
+    type: "MP",
+    value: 9,
+    weight: 6,
+  },
+  {
+    stat: "9% Def",
+    type: "DEF",
+    value: 9,
+    weight: 4,
+  },
+  {
+    stat: "6% ALL STATS",
+    type: "AS",
+    value: 6,
+    weight: 4,
+  },
+  {
+    stat: "STR +1 per 10 Character Levels",
+    type: "STRTEN",
+    value: 1,
+    weight: 1,
+  },
+  {
+    stat: "DEX +1 per 10 Character Levels",
+    type: "DEXTEN",
+    value: 1,
+    weight: 1,
+  },
+  {
+    stat: "INT +1 per 10 Character Levels",
+    type: "INTTEN",
+    value: 1,
+    weight: 1,
+  },
+  {
+    stat: "LUK +1 per 10 Character Levels",
+    type: "LUKTEN",
+    value: 1,
+    weight: 1,
+  },
+  {
+    stat: "5% chance to ignore 20% damage when hit",
+    type: "Chance to Ignore DMG",
+    value: 20,
+    weight: 4,
+  },
+  {
+    stat: "5% chance to ignore 40% damage when hit",
+    type: "Chance to Ignore DMG",
+    value: 40,
+    weight: 4,
+  },
+  {
+    stat: "Increase efficiency of HP recovery items and skills by 30%",
+    type: "REC",
+    value: 30,
+    weight: 4,
+  },
+  {
+    stat: "Decent Sharp Eyes",
+    type: "Decent Skill",
+    value: 1,
+    weight: 4,
+  },
+];
+
+export const uniqueCapeShoulderBeltLines = [
+  {
+    stat: "9% Str",
+    type: "STR",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Dex",
+    type: "DEX",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Int",
+    type: "INT",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Luk",
+    type: "LUK",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% HP",
+    type: "HP",
+    value: 9,
+    weight: 6,
+  },
+  {
+    stat: "9% MP",
+    type: "MP",
+    value: 9,
+    weight: 6,
+  },
+  {
+    stat: "9% Def",
+    type: "DEF",
+    value: 9,
+    weight: 4,
+  },
+  {
+    stat: "6% ALL STATS",
+    type: "AS",
+    value: 6,
+    weight: 4,
+  },
+  {
+    stat: "5% chance to ignore 20% damage when hit",
+    type: "Chance to Ignore DMG",
+    value: 20,
+    weight: 4,
+  },
+  {
+    stat: "5% chance to ignore 40% damage when hit",
+    type: "Chance to Ignore DMG",
+    value: 40,
+    weight: 4,
+  },
+  {
+    stat: "Increase efficiency of HP recovery items and skills by 30%",
+    type: "REC",
+    value: 30,
+    weight: 4,
+  },
+];
+
+//Accessories
+
+export const uniqueAccessoryLines = [
+  {
+    stat: "9% Str",
+    type: "STR",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Dex",
+    type: "DEX",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Int",
+    type: "INT",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Luk",
+    type: "LUK",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% HP",
+    type: "HP",
+    value: 9,
+    weight: 6,
+  },
+  {
+    stat: "9% MP",
+    type: "MP",
+    value: 9,
+    weight: 6,
+  },
+  {
+    stat: "9% Def",
+    type: "DEF",
+    value: 9,
+    weight: 4,
+  },
+  {
+    stat: "6% ALL STATS",
+    type: "AS",
+    value: 6,
+    weight: 4,
+  },
+  {
+    stat: "Increase efficiency of HP recovery items and skills by 30%",
+    type: "REC",
+    value: 30,
+    weight: 4,
+  },
+];
+
+//Heart
+
+export const uniqueHeartLines = [
+  {
+    stat: "9% Str",
+    type: "STR",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Dex",
+    type: "DEX",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Int",
+    type: "INT",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Luk",
+    type: "LUK",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% HP",
+    type: "HP",
+    value: 9,
+    weight: 6,
+  },
+  {
+    stat: "9% MP",
+    type: "MP",
+    value: 9,
+    weight: 6,
+  },
+  {
+    stat: "9% Def",
+    type: "DEF",
+    value: 9,
+    weight: 4,
+  },
+  {
+    stat: "6% ALL STATS",
+    type: "AS",
+    value: 6,
+    weight: 4,
+  },
+  {
+    stat: "Increase efficiency of HP recovery items and skills by 30%",
+    type: "REC",
+    value: 30,
+    weight: 4,
+  },
+];
+
+//Weapon
+
+export const uniqueWeaponLines = [
+  {
+    stat: "9% Str",
+    type: "STR",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Dex",
+    type: "DEX",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Int",
+    type: "INT",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Luk",
+    type: "LUK",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% ATT",
+    type: "ATT",
+    value: 9,
+    weight: 3,
+  },
+  {
+    stat: "9% MATT",
+    type: "MATT",
+    value: 9,
+    weight: 3,
+  },
+  {
+    stat: "9% Critical Chance",
+    type: "Crit Chance",
+    value: 9,
+    weight: 4,
+  },
+  {
+    stat: "9% Damage",
+    type: "DMG",
+    value: 9,
+    weight: 3,
+  },
+  {
+    stat: "6% ALL STATS",
+    type: "AS",
+    value: 6,
+    weight: 4,
+  },
+  {
+    stat: "Ignore Enemy Defense +30%",
+    type: "IED",
+    value: 30,
+    weight: 3,
+  },
+  {
+    stat: "Damage when attacking boss monsters +30%",
+    type: "BOSS",
+    value: 30,
+    weight: 3,
+  },
+];
+
+export const uniqueSecondaryLines = [
+  {
+    stat: "9% Str",
+    type: "STR",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Dex",
+    type: "DEX",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Int",
+    type: "INT",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Luk",
+    type: "LUK",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% ATT",
+    type: "ATT",
+    value: 9,
+    weight: 3,
+  },
+  {
+    stat: "9% MATT",
+    type: "MATT",
+    value: 9,
+    weight: 3,
+  },
+  {
+    stat: "9% Critical Chance",
+    type: "Crit Chance",
+    value: 9,
+    weight: 4,
+  },
+  {
+    stat: "9% Damage",
+    type: "DMG",
+    value: 9,
+    weight: 3,
+  },
+  {
+    stat: "6% ALL STATS",
+    type: "AS",
+    value: 6,
+    weight: 4,
+  },
+  {
+    stat: "Ignore Enemy Defense +30%",
+    type: "IED",
+    value: 30,
+    weight: 3,
+  },
+  {
+    stat: "5% chance to ignore 20% dmg when hit",
+    type: "Chance to Ignore DMG",
+    value: 20,
+    weight: 4,
+  },
+  {
+    stat: "5% chance to ignore 40% dmg when hit",
+    type: "Chance to Ignore DMG",
+    value: 40,
+    weight: 4,
+  },
+  {
+    stat: "Damage when attacking boss monsters +30%",
+    type: "BOSS",
+    value: 30,
+    weight: 3,
+  },
+];
+
+export const uniqueEmblemLines = [
+  {
+    stat: "9% Str",
+    type: "STR",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Dex",
+    type: "DEX",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Int",
+    type: "INT",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% Luk",
+    type: "LUK",
+    value: 9,
+    weight: 5,
+  },
+  {
+    stat: "9% ATT",
+    type: "ATT",
+    value: 9,
+    weight: 3,
+  },
+  {
+    stat: "9% MATT",
+    type: "MATT",
+    value: 9,
+    weight: 3,
+  },
+  {
+    stat: "9% Critical Chance",
+    type: "Crit Chance",
+    value: 9,
+    weight: 4,
+  },
+  {
+    stat: "9% Damage",
+    type: "DMG",
+    value: 9,
+    weight: 3,
+  },
+  {
+    stat: "6% ALL STATS",
+    type: "AS",
+    value: 6,
+    weight: 4,
+  },
+  {
+    stat: "Ignore Enemy Defense +30%",
+    type: "IED",
+    value: 30,
+    weight: 3,
+  },
+];
+
+// const lines = "lines";
+
+// export default lines;
